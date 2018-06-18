afd165cc3323f4fcaf8697067d451e4cbc409c7d
Jacques Swanepoel <JacquesS@CF.co.za>
2018-06-18 12:14:32 +0200

    Add project files.

---
+++ Bh.TechnicalAssessment.Contacts/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bh.TechnicalAssessment.Contacts.Models
+{
+    [Table("Contacts")]
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(96)]
+        public string Firstname { get; set; }
+        [Required]
+        [StringLength(96)]
+        public string Lastname { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Phone { get; set; }
+        [StringLength(10)]
+        public string Email { get; set; }
+        [StringLength(255)]
+        public string Note { get; set; }
+    }
+}
---
+++ Bh.TechnicalAssessment.Contacts/Persistence/BhDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Bh.TechnicalAssessment.Contacts.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bh.TechnicalAssessment.Contacts.Persistence
+{
+    public class BhDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public BhDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
